eb3d95c2146978c253a36403d2f7107a3e820b36
Ashwoq_s_ <ashwoq00786@gmail.com>
2025-09-29 19:39:00 +0530

    Kids With the Greatest Number of Candies

--- leetCode75.js
+++ leetCode75.js
@@ -50,4 +50,23 @@ function gcdOfString(str1, str2) {
   return str1.slice(0, slicingLength);
 }
 
-console.log(gcdOfString("ABCABCABCABC", "ABC"));
+// console.log(gcdOfString("ABCABCABCABC", "ABC"));
+
+// 1431. Kids With the Greatest Number of Candies
+
+// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+// Note that multiple kids can have the greatest number of candies.
+
+function kidsWithCandies(candies, extraCandies) {
+  let output = [];
+  for (let i = 0; i < candies.length; i++) {
+    output[i] = candies.every((x) => candies[i] + extraCandies >= x);
+  }
+
+  return output;
+}
+
+console.log(kidsWithCandies([4, 2, 1, 1, 2], 1));
