6eb35dbcae39b6078bc22f1d549c6c6eec25210a
Ashwoq_s_ <ashwoq00786@gmail.com>
2025-09-29 18:45:18 +0530

    Greatest Common Divisor of Strings

--- leetCode75.js
+++ leetCode75.js
@@ -30,4 +30,24 @@ const mergeStringAlternate = function (word1, word2) {
   return merged.filter((x) => x.trim("") !== "").join("");
 };
 
-console.log(mergeStringAlternate("ab", "pqrs"));
+// console.log(mergeStringAlternate("ab", "pqrs"));
+
+//1071. Greatest Common Divisor of Strings
+// For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+function gcdOfString(str1, str2) {
+  if (str1 + str2 !== str2 + str1) return "";
+
+  function gcd(a, b) {
+    if (b === 0) return a;
+    else return gcd(b, a % b);
+  }
+
+  const slicingLength = gcd(str1.length, str2.length);
+
+  return str1.slice(0, slicingLength);
+}
+
+console.log(gcdOfString("ABCABCABCABC", "ABC"));
