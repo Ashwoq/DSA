0cb482c916c37de2803e0d8cc0c3193547ef4a0d
Ashwoq_s_ <ashwoq00786@gmail.com>
2025-09-30 20:36:36 +0530

    Can Place Flowers

--- leetCode75.js
+++ leetCode75.js
@@ -69,4 +69,29 @@ function kidsWithCandies(candies, extraCandies) {
   return output;
 }
 
-console.log(kidsWithCandies([4, 2, 1, 1, 2], 1));
+// console.log(kidsWithCandies([4, 2, 1, 1, 2], 1));
+
+// 605. Can Place Flowers
+
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+function canPlaceFlowers(flowerbed, n) {
+  let count = 0;
+
+  for (let i = 0; i < flowerbed.length; i++) {
+    let prev = i === 0 || flowerbed[i - 1] === 0;
+    let next = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+
+    if (prev && flowerbed[i] === 0 && next) {
+      flowerbed[i] = 1;
+      count++;
+      if (count >= n) return true;
+    }
+  }
+
+  return count >= n;
+}
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1));
