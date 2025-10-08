fc3b56549971d46fc001b7d5824b6c16963773e9
Ashwoq_s_ <ashwoq00786@gmail.com>
2025-10-08 23:44:58 +0530

    Find the Difference of Two Arrays

--- leetCode75.js
+++ leetCode75.js
@@ -368,4 +368,25 @@ const isIncreasingTriplet = function (nums) {
   return false;
 };
 
-console.log(isIncreasingTriplet([5, 4, 3, 2, 1]));
+// console.log(isIncreasingTriplet([5, 4, 3, 2, 1]));
+
+// 2215. Find the Difference of Two Arrays
+
+// Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+// answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+// answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+// Note that the integers in the lists may be returned in any order.
+
+const findDifference = function (nums1, nums2) {
+  let set1 = new Set(nums1);
+  let set2 = new Set(nums2);
+
+  let dif1 = [...set1].filter((num) => !set2.has(num));
+
+  let dif2 = [...set2].filter((num) => !set1.has(num));
+
+  return [dif1, dif2];
+};
+
+console.log(findDifference([1, 2, 3], [2, 4, 6]));
