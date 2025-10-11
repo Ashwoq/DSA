d6e68dd568b57cb452753f068071b797910ba322
Ashwoq_s_ <ashwoq00786@gmail.com>
2025-10-11 22:49:48 +0530

    2620. Counter

--- randomLeet.js
+++ randomLeet.js
@@ -75,7 +75,7 @@ const isPalindrome = function (x) {
   // return String(x) === String(x).split("").reverse().join("");
 };
 
-console.log(isPalindrome(121));
+// console.log(isPalindrome(121));
 
 /*
 newlist.append() -> 121%10 -> 1
@@ -95,3 +95,19 @@ n = Math.floor(n/10)
 
 }
 */
+
+// 2620. Counter
+
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+const createCounter = function (n) {
+  return function () {
+    // n++;
+    return n++;
+  };
+};
+
+const counter = createCounter(10);
+console.log(counter()); // 10
+console.log(counter()); // 11
+console.log(counter()); // 12
